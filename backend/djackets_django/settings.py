c79e787c567f6b95f63cca923560525bbd632624
Caio Bianchi <caiohbs23@gmail.com>
2023-09-15 12:48:56 -0300

    progress in frontend and backend

--- backend/djackets_django/settings.py
+++ backend/djackets_django/settings.py
@@ -42,7 +42,9 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
-    'djoser'
+    'djoser',
+    # Django apps
+    'product',
 ]
 
 CORS_ALLOWED_ORIGINS = [
@@ -127,8 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
