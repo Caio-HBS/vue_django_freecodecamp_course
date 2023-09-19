b7608c938e1b811d7455e7b6d988aff97ccbc0c7
Caio Bianchi <caiohbs23@gmail.com>
2023-09-19 17:21:55 -0300

    search bar implementation

--- backend/product/urls.py
+++ backend/product/urls.py
@@ -4,6 +4,7 @@ from product import views
 
 urlpatterns = [
     path('latest-products/', views.LatestProductsListAPIView.as_view()),
+    path('products/search/', views.search),
     path('products/<slug:category_slug>/<slug:product_slug>/', views.ProductDetailAPIView.as_view()),
-    path('products/<slug:category_slug>/', views.CategoryDetailAPIView.as_view())
+    path('products/<slug:category_slug>/', views.CategoryDetailAPIView.as_view()),
 ]
